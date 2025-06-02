bd1b9903267b9a008a2e639465197ece2ba26b3c
Marginall <mistmee2016@gmail.com>
2025-06-02 22:32:17 +0300

    test ci

--- app/components/header/view.tsx
+++ app/components/header/view.tsx
@@ -17,6 +17,7 @@ import { UiSvgIcon } from '@/_ui/components/svg/icon';
 export async function Header(): Promise<ReactElement> {
 	const { logo, navCategories, weather, currency, trendTags, socials } = await useHeader();
 	const t = await getI18n();
+	const testCi = '';
 
 	return (
 		<>
